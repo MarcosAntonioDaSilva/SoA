38b583c2c195f50188af453fc310295a59677e9f
MarcosAntonioDaSilva <marcos.teci9@outlook.com>
2022-09-06 17:06:03 -0300

    Add files via upload
    Arquivo do menu

---
+++ ADM/app/src/main/java/Arquitetura/SoA/adm/MainActivity.kt
@@ -0,0 +1,11 @@
+package Arquitetura.SoA.adm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
